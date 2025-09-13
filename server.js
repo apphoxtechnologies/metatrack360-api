0d0f37ea47609b98870237ffd91323e1fd9bfac4
apphoxtechnologies <apphoxtechnologies@gmail.com>
2025-09-13 21:17:47 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const db = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 3, // <-- UPDATED from 5 to 3 to reduce connection usage
+    connectionLimit: 1, // <-- UPDATED to absolute minimum to avoid hitting server limits
     queueLimit: 0
 }).promise();
 
